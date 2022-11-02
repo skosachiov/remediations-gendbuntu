0c0ff17469010ccba3470a3b17ab9cf85541cf5e
skosachiov <skosachiov@gmail.com>
2022-11-02 12:16:24 +0300

    check subnets

--- utils/vars-to-subnets.py
+++ utils/vars-to-subnets.py
@@ -1,11 +1,14 @@
 import yaml
 import os
+from ipaddress import *
 
 rootdir = 'inventories/subnets'
 suffix = 'vars/main.yml'
 outfile = 'inventories/subnets/subnets.yml'
 
 subnets = {"var_subnets": {}}
+
+ip_nets = []
 
 for file in os.listdir(rootdir):
     folder = os.path.join(rootdir, file)
@@ -17,10 +20,17 @@ for file in os.listdir(rootdir):
                 # print(d)
                 if "var_subnets" in d.keys():
                     subnets["var_subnets"][file] = d["var_subnets"]
+                    # check subnets
+                    for n in d["var_subnets"]:
+                        for ip_net in ip_nets:
+                            if ip_network(ip_net[0]).subnet_of(ip_network(n)):
+                                print(file, n, "subnet_of", ip_net[1], ip_net[0])
+                            if ip_network(ip_net[0]).supernet_of(ip_network(n)):
+                                print(file, n, "supernet_of", ip_net[1], ip_net[0])
+                        ip_nets += [(n, file)]
             except yaml.YAMLError as e:
                 print(e)
 with open(outfile, 'w') as f:
     f.write("# Please do not modify this file manually, use the script from the utils folder.\n\n")
     f.write(yaml.dump(subnets, default_flow_style=False))
-
             
