e451c3f19b0e1e4bcaa104a366a8f3b543db9afe
skosachiov <skosachiov@gmail.com>
2023-06-25 10:28:56 +0300

    add push template

---
+++ utils/unique-tasks/at-template.sh
@@ -0,0 +1,8 @@
+#
+# One-liner that applies tasks to thousands of hosts (/tmp/hosts) under the timeout utility control.
+# The script splits the list of hosts into chunks (25) and controls the execution of each chunk until the timeout (600s) expires.
+# If the timeout is exceeded, the timeout utility kills the ansible-playbook on this iteration.
+# This avoids stopping the ansible-playbook due to defunct friezes.
+#
+
+ANSIBLE_HOST_KEY_CHECKING=False /bin/bash -c 'while mapfile -n 25 ary && ((${#ary[@]})); do echo "${ary[@]}" | tr -d " " > /tmp/hosts.chunk; timeout 600 /usr/bin/ansible-playbook -v -b -i /tmp/hosts.chunk -u ansible -e "var_url_deb=https://server/files/pkg.deb" /var/www/html/git/remediations-gendbuntu/utils/unique-tasks/install-url-deb.yml; done < /tmp/hosts'
