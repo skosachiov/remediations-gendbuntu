efe19cebc138a9e9fc5bb187b11b5a232df074b4
skosachiov <skosachiov@gmail.com>
2025-11-03 18:06:31 +0300

    init secure boot

--- assets/late-command.sh
+++ assets/late-command.sh
@@ -7,7 +7,7 @@ sed -i "s/127.0.1.1.*/127.0.1.1\t$HOSTNAME/" /etc/hosts
 	
 sed -i "s/deb cdrom/# deb cdrom/" /etc/apt/sources.list
 echo 'XKBMODEL="pc105"
-XKBLAYOUT="us,fr"
+XKBLAYOUT="us,gb"
 XKBVARIANT=","
 XKBOPTIONS=""
 BACKSPACE="guess"' > /etc/default/keyboard
@@ -26,7 +26,7 @@ echo insecure | cryptsetup luksAddKey $CRYPTPART /tmp/unlock-key-file
 systemd-cryptenroll --unlock-key-file=/tmp/unlock-key-file --tpm2-device=auto --tpm2-pcrs=0+1+2 $CRYPTPART
 cryptsetup --key-file=/tmp/unlock-key-file luksKillSlot $CRYPTPART 0
 rm -f /boot/efi/unlock-key-file.gpg
-curl -o /tmp/sample-pubkey.asc https://github.com/skosachiov/remediations-gendbuntu/raw/main/assets/sample-pubkey.asc
+wget -o /tmp/sample-pubkey.asc https://github.com/skosachiov/remediations-gendbuntu/raw/main/assets/sample-pubkey.asc
 gpg --encrypt --recipient-file /tmp/sample-pubkey.asc --output /boot/efi/unlock-key-file.gpg < /tmp/unlock-key-file
 echo add_dracutmodules+=\" tpm2-tss crypt \" > /etc/dracut.conf
 sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="rd.auto rd.luks=1"/' /etc/default/grub
