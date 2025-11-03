fc9403a92f252b95d2c8f556674ee6b824161550
skosachiov <skosachiov@gmail.com>
2025-11-03 14:22:37 +0300

    init secure boot

--- assets/late-command.sh
+++ assets/late-command.sh
@@ -26,7 +26,8 @@ echo insecure | cryptsetup luksAddKey $CRYPTPART /tmp/unlock-key-file
 systemd-cryptenroll --unlock-key-file=/tmp/unlock-key-file --tpm2-device=auto --tpm2-pcrs=0+1+2 $CRYPTPART
 cryptsetup --key-file=/tmp/unlock-key-file luksKillSlot $CRYPTPART 0
 rm -f /boot/efi/unlock-key-file.gpg
-gpg --encrypt --recipient-file /etc/apt/keyrings/corpos_repo.asc --output /boot/efi/unlock-key-file.gpg < /tmp/unlock-key-file
+curl -o /tmp/sample-pubkey.asc https://github.com/skosachiov/remediations-gendbuntu/raw/main/assets/sample-pubkey.asc
+gpg --encrypt --recipient-file /tmp/sample-pubkey.asc --output /boot/efi/unlock-key-file.gpg < /tmp/unlock-key-file
 echo add_dracutmodules+=\" tpm2-tss crypt \" > /etc/dracut.conf
 sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="rd.auto rd.luks=1"/' /etc/default/grub
 sed -i "s/^/# /"  /etc/crypttab
